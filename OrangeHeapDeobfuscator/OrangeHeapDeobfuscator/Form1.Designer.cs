be70ab59c3f626d9fb914b81bb98b3208f949897
lool <34941099+netlool@users.noreply.github.com>
2018-03-31 13:33:55 +0200

    Add files via upload

---
+++ OrangeHeapDeobfuscator/OrangeHeapDeobfuscator/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace OrangeHeapDeobfuscator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbin = new System.Windows.Forms.TextBox();
+            this.btnofd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnsfd = new System.Windows.Forms.Button();
+            this.tbout = new System.Windows.Forms.TextBox();
+            this.btnDeobf = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbin
+            // 
+            this.tbin.Location = new System.Drawing.Point(12, 37);
+            this.tbin.Multiline = true;
+            this.tbin.Name = "tbin";
+            this.tbin.Size = new System.Drawing.Size(541, 30);
+            this.tbin.TabIndex = 0;
+            // 
+            // btnofd
+            // 
+            this.btnofd.Location = new System.Drawing.Point(560, 37);
+            this.btnofd.Name = "btnofd";
+            this.btnofd.Size = new System.Drawing.Size(75, 30);
+            this.btnofd.TabIndex = 1;
+            this.btnofd.Text = "...";
+            this.btnofd.UseVisualStyleBackColor = true;
+            this.btnofd.Click += new System.EventHandler(this.btnofd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output";
+            // 
+            // btnsfd
+            // 
+            this.btnsfd.Location = new System.Drawing.Point(560, 100);
+            this.btnsfd.Name = "btnsfd";
+            this.btnsfd.Size = new System.Drawing.Size(75, 30);
+            this.btnsfd.TabIndex = 4;
+            this.btnsfd.Text = "...";
+            this.btnsfd.UseVisualStyleBackColor = true;
+            this.btnsfd.Click += new System.EventHandler(this.btnsfd_Click);
+            // 
+            // tbout
+            // 
+            this.tbout.Location = new System.Drawing.Point(12, 100);
+            this.tbout.Multiline = true;
+            this.tbout.Name = "tbout";
+            this.tbout.Size = new System.Drawing.Size(541, 30);
+            this.tbout.TabIndex = 3;
+            // 
+            // btnDeobf
+            // 
+            this.btnDeobf.Location = new System.Drawing.Point(12, 164);
+            this.btnDeobf.Name = "btnDeobf";
+            this.btnDeobf.Size = new System.Drawing.Size(623, 76);
+            this.btnDeobf.TabIndex = 6;
+            this.btnDeobf.Text = "Deobfuscate Assembly";
+            this.btnDeobf.UseVisualStyleBackColor = true;
+            this.btnDeobf.Click += new System.EventHandler(this.btnDeobf_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 262);
+            this.Controls.Add(this.btnDeobf);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnsfd);
+            this.Controls.Add(this.tbout);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnofd);
+            this.Controls.Add(this.tbin);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "OrangeHeap Deobfuscator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbin;
+        private System.Windows.Forms.Button btnofd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnsfd;
+        private System.Windows.Forms.TextBox tbout;
+        private System.Windows.Forms.Button btnDeobf;
+    }
+}
+
---
+++ OrangeHeapDeobfuscator/OrangeHeapDeobfuscator/Form1.cs
@@ -0,0 +1,119 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrangeHeapDeobfuscator
+{
+    public partial class MainForm : Form
+    {
+        private static MethodDef target;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        string NAME
+        {
+        get
+            {
+                return "OrangeHeap Deobfuscator";
+            }
+        }
+        private void btnofd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "NET Assembly| *.exe| NET DLL| *.dll";
+            ofd.Title = "Choose a File";
+            if(ofd.ShowDialog() == DialogResult.OK)
+                tbin.Text = ofd.FileName;
+        }
+
+        private void btnsfd_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Choose a File";
+            sfd.Filter = "NET Assembly| *.exe| NET DLL| *.dll";
+            if (sfd.ShowDialog() == DialogResult.OK) 
+                tbout.Text = sfd.FileName;
+        }
+
+        private void btnDeobf_Click(object sender, EventArgs e)
+        {
+            ModuleDefMD md = ModuleDefMD.Load(tbin.Text);
+            if (!IsObfuscator(md))
+                MessageBox.Show(null, "This Assembly is not protected with Orange Heap!", NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                foreach(var type in md.Types)
+                {
+                    foreach(var method in type.Methods)
+                    {
+                        if (!method.HasBody) continue;
+                        CilBody body = method.Body;
+                        body.KeepOldMaxStack = true;
+                        for(int  i = 0; i < body.Instructions.Count; i++)
+                        {
+                            if(body.Instructions[i].OpCode == OpCodes.Ldstr &&
+                                body.Instructions[i + 1].OpCode == OpCodes.Call &&
+                                body.Instructions[i + 1].Operand == target)
+                            {
+                                body.Instructions[i].Operand = decryptstring(body.Instructions[i].Operand.ToString());
+                                body.Instructions[i + 1].Operand = null;
+                                body.Instructions[i + 1].OpCode = OpCodes.Nop;
+                            }
+                        }
+                    }
+                }
+                md.Write(tbout.Text);
+                MessageBox.Show(null,"Saved Assembly to: " + tbout.Text, NAME, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+              
+        }
+        private static bool IsObfuscator(ModuleDefMD md)
+        {
+            foreach(var type in md.Types)
+            {
+                foreach(var method in type.Methods)
+                {
+                    if (!method.HasBody) continue;
+                    if (!method.ReturnType.ToString().ToLower().Contains("string")) continue;
+                    CilBody body = method.Body;
+                    for(int i = 0; i < body.Instructions.Count; i++)
+                    {
+                       if(body.Instructions[i].OpCode == OpCodes.Shr &&
+                            body.Instructions[i + 1].OpCode == OpCodes.Ldloc_2 &&
+                            body.Instructions[i + 2].OpCode == OpCodes.Xor &&
+                            body.Instructions[i + 3].OpCode == OpCodes.Conv_U1)
+                        {
+                            target = method;
+                            return true;
+                        }  
+                    }
+                }
+            }
+            return false;
+        }
+        private static string decryptstring(string a6)
+        {
+            int length = a6.Length;
+            char[] array = new char[length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                char c = a6[i];
+                byte b = (byte)((int)c ^ length - i);
+                byte b2 = (byte)((int)(c >> 8) ^ i);
+                array[i] = (char)((int)b2 << 8 | (int)b);
+            }
+            return string.Intern(new string(array));
+        }
+     
+        } 
+    }
+
